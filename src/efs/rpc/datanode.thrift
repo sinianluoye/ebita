966b2f89a87fa9fafe49c4792e0c75f439310937
sinianluoye <sinianluoye@outlook.com>
2022-03-30 23:32:06 +0800

    fix

--- src/efs/rpc/datanode.thrift
+++ src/efs/rpc/datanode.thrift
@@ -9,6 +9,6 @@ service DataNode {
      * return: 读取到的数据
      */
     binary read(1:int block_id, 2:int offset, 3:int length) throws (1: EfsException e),
-    bool write(`:int block_id, 2:int offset, 3:int length, 4:binary data) throws(1: EfsException e)
+    bool write(1:int block_id, 2:int offset, 3:int length, 4:binary data) throws(1: EfsException e)
 
 }
